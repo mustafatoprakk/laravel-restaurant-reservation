8254c297689bc8d38505f2db5a8187bf1d19e5d5
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-04-27 02:40:19 +0300

    menu ve category relationship ilişkisi yapıldı ve menu de category alanı eklendi

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,9 +9,14 @@ class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable=[
+    protected $fillable = [
         "name",
         "image",
         "description"
     ];
+
+    public function menus()
+    {
+        return $this->belongsToMany(Menu::class, "category_menu");
+    }
 }
