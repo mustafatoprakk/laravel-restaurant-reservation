e5c2fe8456478b8e2408b3e13fea213f74b804ea
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-04-27 12:32:48 +0300

    table crud yapıldı ve enums ile location ve status select ile gösterildi

--- resources/views/admin/tables/edit.blade.php
+++ resources/views/admin/tables/edit.blade.php
@@ -8,26 +8,27 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex  m-2 p-2">
-                <a href="{{ route('admin.tables.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back Table</a>
+                <a href="{{ route('admin.tables.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back Menu</a>
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2.mt-10">
-                    <form action="{{ route('admin.tables.store') }}" method="POST">
+                    <form method="POST" action="{{ route('admin.tables.update',$table->id) }}" >
                         @csrf
+                        @method("PUT")
                         <div class="mb-6">
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required="">
+                            <input type="text" name="name" id="name" value="{{ $table->name }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required="">
                         </div>
                         <div class="mb-6">
                             <label for="guest_number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Guest Number</label>
-                            <input type="number" name="guest_number" id="guest_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required="">
+                            <input value="{{ $table->guest_number }}" type="number" name="guest_number" id="guest_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required="">
                         </div>
                         <div class="mb-6">
                             <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Status</label>
                             <div class="mt-1">
                                 <select name="status" id="status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     @foreach (App\Enums\TableStatus::cases() as $status)
-                                        <option value="{{ $status->value }}">{{ $status->name }}</option>
+                                        <option value="{{ $status->value }}" @selected($table->status->value==$status->value)>{{ $status->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -37,12 +38,12 @@
                             <div class="mt-1">
                                 <select name="location" id="location" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     @foreach (App\Enums\TableLocation::cases() as $location)
-                                        <option value="{{ $location->value }}">{{ $location->name }}</option>
+                                        <option value="{{ $location->value }}" @selected($table->location->value==$location->value)>{{ $location->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
                         </div>
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Store</button>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
                     </form>
                 </div>
             </div>
--- resources/views/admin/tables/index.blade.php
+++ resources/views/admin/tables/index.blade.php
@@ -15,16 +15,16 @@
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                 <th scope="col" class="px-6 py-3">
-                Product name
+                Name
                 </th>
                 <th scope="col" class="px-6 py-3">
-                Color
+                Guest Number
                 </th>
                 <th scope="col" class="px-6 py-3">
-                Category
+                Status
                 </th>
                 <th scope="col" class="px-6 py-3">
-                Price
+                Location
                 </th>
                 <th scope="col" class="px-6 py-3">
                 <span class="sr-only">Edit</span>
@@ -32,57 +32,32 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                Sliver
-                </td>
-                <td class="px-6 py-4">
-                Laptop
-                </td>
-                <td class="px-6 py-4">
-                $2999
-                </td>
-                <td class="px-6 py-4 text-right">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                White
-                </td>
-                <td class="px-6 py-4">
-                Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                $1999
-                </td>
-                <td class="px-6 py-4 text-right">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                Black
-                </td>
-                <td class="px-6 py-4">
-                Accessories
-                </td>
-                <td class="px-6 py-4">
-                $99
-                </td>
-                <td class="px-6 py-4 text-right">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-                </tr>
+                    @foreach ($tables as $table)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{ $table->name }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $table->guest_number }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $table->status->name }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $table->location->name }}
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <div class="flex space-x-2">
+                                <a href="{{ route('admin.tables.edit',$table->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-2">Edit</a>
+                                <form action="{{ route('admin.tables.destroy',$table->id) }}" method="post" onsubmit="return confirm('Are you sure?')">
+                                    @csrf
+                                    @method("DELETE")
+                                    <button type="submit">Delete</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
                 </tbody>
                 </table>
                 </div>
