a543ab28e4c6561e7a9629a23ed062fd31a44a51
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-04-27 16:32:39 +0300

    reservation create ve read yapıldı

--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -8,22 +8,45 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex  m-2 p-2">
-                <a href="{{ route('admin.reservations.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back Reservation</a>
+                <a href="{{ route('admin.reservations.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back Table</a>
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2.mt-10">
-                    <form>
+                    <form action="{{ route('admin.reservations.store') }}" method="POST">
+                        @csrf
                         <div class="mb-6">
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
-                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required="">
+                            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First Name</label>
+                            <input type="text" name="first_name" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="First Name" required="">
                         </div>
                         <div class="mb-6">
-                            <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your image</label>
-                            <input type="file" name="image" id="image" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required="">
+                            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last Name</label>
+                            <input type="text" name="last_name" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Last Name" required="">
                         </div>
                         <div class="mb-6">
-                            <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your description</label>
-                            <textarea id="description" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a description..."></textarea>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@gmail.com" required="">
+                        </div>
+                        <div class="mb-6">
+                            <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Phone</label>
+                            <input type="text" name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="545 222 32 32" required="">
+                        </div>
+                        <div class="mb-6">
+                            <label for="res_date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Reservation Date</label>
+                            <input type="datetime-local" name="res_date" id="res_date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required="">
+                        </div>
+                        <div class="mb-6">
+                            <label for="guest_number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Guest Number</label>
+                            <input type="number" name="guest_number" id="guest_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required="">
+                        </div>
+                        <div class="mb-6">
+                            <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Table</label>
+                            <div class="mt-1">
+                                <select name="table_id" id="table_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    @foreach ($tables as $table)
+                                        <option value="{{ $table->id }}">{{ $table->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
                         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Store</button>
                     </form>
--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -15,16 +15,25 @@
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                 <th scope="col" class="px-6 py-3">
-                Product name
+                First Name
                 </th>
                 <th scope="col" class="px-6 py-3">
-                Color
+                Last Name
                 </th>
                 <th scope="col" class="px-6 py-3">
-                Category
+                Email
                 </th>
                 <th scope="col" class="px-6 py-3">
-                Price
+                Phone
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Reservation Date
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Table Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Guest Number
                 </th>
                 <th scope="col" class="px-6 py-3">
                 <span class="sr-only">Edit</span>
@@ -32,57 +41,41 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                Sliver
-                </td>
-                <td class="px-6 py-4">
-                Laptop
-                </td>
-                <td class="px-6 py-4">
-                $2999
-                </td>
-                <td class="px-6 py-4 text-right">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                White
-                </td>
-                <td class="px-6 py-4">
-                Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                $1999
-                </td>
-                <td class="px-6 py-4 text-right">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                Black
-                </td>
-                <td class="px-6 py-4">
-                Accessories
-                </td>
-                <td class="px-6 py-4">
-                $99
-                </td>
-                <td class="px-6 py-4 text-right">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-                </tr>
+                    @foreach ($reservations as $reservation)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{ $reservation->first_name }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $reservation->last_name }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $reservation->email }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $reservation->phone }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $reservation->res_date }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $reservation->table_id }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $reservation->guest_number }}
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <div class="flex space-x-2">
+                                <a href="{{ route('admin.reservations.edit',$reservation->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-2">Edit</a>
+                                <form action="{{ route('admin.reservations.destroy',$reservation->id) }}" method="post" onsubmit="return confirm('Are you sure?')">
+                                    @csrf
+                                    @method("DELETE")
+                                    <button type="submit">Delete</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
                 </tbody>
                 </table>
                 </div>
