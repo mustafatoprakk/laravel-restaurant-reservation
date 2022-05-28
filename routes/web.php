b36d683662db2ac25fba6ce177e8c95ab144156a
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-05-28 14:46:44 +0300

    routes yapıldı

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\ReservationController;
 use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Frontend\CategoryController as FrontendCategoryController;
+use App\Http\Controllers\Frontend\MenuController as FrontendMenuController;
+use App\Http\Controllers\Frontend\ReservationController as FrontendReservationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,17 +25,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("/categories", [FrontendCategoryController::class, "index"])->name("categories.index");
+Route::get("/categories/{categoty}", [FrontendCategoryController::class, "show"])->name("categories.index");
+Route::get("/menus", [FrontendMenuController::class, "index"])->name("menus.index");
+Route::get("/reservation/step-one", [FrontendReservationController::class, "stepOne"])->name("reservations.step.one");
+Route::get("/reservation/step-two", [FrontendReservationController::class, "stepTwo"])->name("reservations.step.two");
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::middleware(["auth","admin"])->name("admin.")->prefix("admin")->group(function(){
-    Route::get("/",[AdminController::class,"index"])->name("index");
-    
-    Route::resource("/categories",CategoryController::class);
-    Route::resource("/menus",MenuController::class);
-    Route::resource("/tables",TableController::class);
-    Route::resource("/reservations",ReservationController::class);
+Route::middleware(["auth", "admin"])->name("admin.")->prefix("admin")->group(function () {
+    Route::get("/", [AdminController::class, "index"])->name("index");
+
+    Route::resource("/categories", CategoryController::class);
+    Route::resource("/menus", MenuController::class);
+    Route::resource("/tables", TableController::class);
+    Route::resource("/reservations", ReservationController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
